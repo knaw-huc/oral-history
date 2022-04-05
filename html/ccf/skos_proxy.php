34191cfde1fb413bb131a7dcfe8e227d9e292567
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2022-04-05 21:06:48 +0200

    skos proxy added

---
+++ html/ccf/skos_proxy.php
@@ -0,0 +1,23 @@
+<?php
+$ch = curl_init("https://skosmos.sd.di.huc.knaw.nl/rest/v1/" . $_GET["vocabulary"] . "/search?unique=yes&query=" . $_GET["q"] . "*");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/text'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$json = curl_exec($ch);
+//echo $json;
+$json = json_decode($json, true);
+if (!$json) {
+    $json = "";
+}
+
+$results = $json["results"];
+$buffer = array();
+foreach ($results as $result) {
+    $buffer[] = $result["prefLabel"];
+}
+
+$retArray = array("query" => "Unit", "suggestions" => $buffer);
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+echo json_encode($retArray);
+curl_close($ch);
