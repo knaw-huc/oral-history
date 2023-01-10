7d14a2f134ca45dace7c0b579a913ecdfd9c3f5c
Menzo Windhouwer <menzo@windhouwer.nl>
2023-01-10 10:29:29 +0100

    TODOs voor response logica

--- html/ccf/js/src/scanHandler.js
+++ html/ccf/js/src/scanHandler.js
@@ -2,11 +2,32 @@
 
 function scanHandler() {
 
+    // TODO: luister naar data-class=pageCount, zet globale variabele pageCount
 
     console.log($("div[data-class='page']"));
     $("div[data-class='page']").each(function () {
         $(this).on("click", function () {
             showScan($(this).children(1).children(1).children(0).val());
+            //TODO:
+            // we moeten een lijst maken van de pagina's in deze response:
+            // 1. de huidige
+            // 2 ... n-1 alle pagina's zonder kloeke code
+            // n. de eerste van deze response, i.e. de pagina die een kloeke code heeft als we terug gaan in de pagina lijst
+            // EVENT: de huidige pagina wordt actief
+            // dan:
+            //     0. dit is de volgende pagina in de response
+            //     1. de vorige pagina is de pagina met type verwacht
+            //     2. vorige.volgorde < pageCount zet dan huidige.volgorde op vorige.volgorde +1
+            // EVENT: als KloekeCode wordt ingevuld op de huidige pagina
+            // dan: 
+            //     0. we beginnen dus aan een nieuwe response
+            //     1. de eerste verwijst dan naar de vorige response, en we willen huidige.inzending = eerste.inzending +1
+            //     2. dit is de eerste pagina van de response, dus we zetten huidige.volgorde op 1 
+            // EVENT: als huidige.type op losse aanvulling wordt gezet
+            // dan:
+            //     0. dit is nog een extra pagina in de response
+            //     1. de vorige pagina is de pagina met type extra
+            //     2. huidige.volgorde op vorige.volgorde + 1
         });
     })
 }
