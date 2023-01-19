1c7ab79cafcfdfedf0918efdfc93469cdbd06c81
meindertkroese <>
2023-01-19 10:56:08 +0100

    clean up code

--- html/ccf/js/src/scanHandler.js
+++ html/ccf/js/src/scanHandler.js
@@ -8,33 +8,12 @@ function pageCount() {
     console.log("pageCount= " + pageCount);
 }
 
-// this one refuses to work:
-// seems to work???
+// works
 function number(page) {
-//    console.log('find number');
-//    console.log(page);
     try {
-//        console.log('first try');
         return page.find('div[data-name="nr"]').children(1).children(0).val();
     } catch(error) {
-        //try {
-/*            console.log('second try');
-            console.log(page);
-            console.log(page["children"]);
-            console.log(page["children"]["1"]);
-            console.log(page["children"]["1"]["children"]["1"]);
-            console.log(page["children"]["1"]["children"]["1"]["children"]);
-            console.log(page["children"]["1"]["children"]["1"]["children"]["0"]);
-            console.log(page["children"]["1"]["children"]["1"]["children"]["0"]["value"]); */
-            return page["children"]["1"]["children"]["1"]["children"]["0"]["value"];
-//            console.log(page.children(1));
-//            console.log(page.children(1).children(1));
-//            console.log(page.children(1).children(1).children(0));
-//            console.log(page.children(1).children(1).children(0).value);
-//            return page.children(1).children(1).children(0).value;
-        //} catch(error) {
-            //return '';
-        //}
+        return page["children"]["1"]["children"]["1"]["children"]["0"]["value"];
     }
 }
 
@@ -71,7 +50,7 @@ function kloeke(page) {
     }
  }
 
-// doesn't return proper value:
+// works
 function pageType(page) {
     try {
         return page.find('div[data-name="pageType"]')[0].children[1].children[0]["value"];
@@ -81,55 +60,25 @@ function pageType(page) {
         } catch(error) {
             console.log(page);
             console.log(page["children"]);
-//            console.log(page["children"]["5"]);
-//            console.log(page["children"]["5"]["children"]);
-//            console.log(page["children"]["5"]["children"]["1"]);
-//            console.log(page["children"]["5"]["children"]["1"]["children"]);
-//            console.log(page["children"]["5"]["children"]["1"]["children"]["0"]);
-//            console.log(page["children"]["5"]["children"]["1"]["children"]["0"]["value"]);
             return '';
         }
     }
 }
 
 function checkPrevious(currentPage) {
-    console.log("checkPrevious");
     numberCP = number(currentPage);
-    console.log('number(currentPage): ' + numberCP);
     kloekeCode = kloeke(currentPage);
     if (kloekeCode != '') {
         console.log('set volgorde = 1');
         setVolgorde(currentPage, 1);
         return;
     }
-    console.log('kloeke code empty');
+    // kloekecode empty
+
     let pages = $("div[data-class='page']");
     let c = 0;
     let pageFound = false;
-//    for (const prop in pages) {
-//        console.log(`pages.${prop} = ${pages[prop]}`);
-//    }
-    /*
-    for (const element of pages) {
-        console.log(c);
-        console.log(element);
-        console.log(number(element));
-        if (element == currentPage) {
-            console.log('found: ' + c);
-            break;
-        }
-        c += 1;
-    }
-    c = 0;
-*/
-
     for (const page in pages) {
-//        console.log(page);
-//        console.log(pages[c]);
-//        console.log('number(page): ' + number(pages[page]));
-//        console.log(JSON.stringify(page));
-//        console.log(JSON.stringify(currentPage));
-//        if (`${pages[page]}` == currentPage) {
         if (number(pages[page]) == numberCP) {
             pageFound = true;
             break;
@@ -138,7 +87,6 @@ function checkPrevious(currentPage) {
     }
     console.log('pagefound: ' + pageFound);
     console.log('c: ' + c);
-    console.log(pages[c])
 
     //pages[c] is de huidige pagina
     let f = c;
@@ -157,7 +105,7 @@ function checkPrevious(currentPage) {
     console.log('kloekefound: ' + kloekeFound);
     console.log('f: ' + f);
 
-    let v = c;
+    let v = c - 1;
     let verwachtFound = false;
     while (pageType(pages[v])!='verwacht') {
         v -= 1;
@@ -177,37 +125,7 @@ function checkPrevious(currentPage) {
     }
 
     setVolgorde(pages[c], parseInt(volgorde(pages[v])) + 1);
-    console.log('volgorde set???');
-
-
-//    console.log('kloeke code: ' + kloeke(currentPage));
-/*    console.log(kloeke.parentNode);
-    console.log(kloeke.prevObject);
-    console.log(kloeke.prevObject[0]);
-    console.log(kloeke.prevObject[0]['previousSibling']);
-    */
-//    let prevkloekeCode = kloeke.prevObject[0]['previousSibling'].children[1].children[0]["value"];
-//    console.log('prev kc: ' + prevkloekeCode)
-    //console.log(kloeke.find('div[data-class="page"]').div);
-    //console.log(kloeke.find('div[data-class="page"]').div.previousSibling);
-    //console.log(kloeke.find('div[data-class="page"]').prevObject);
-    //console.log(kloeke.find('div[data-class="page"]').prevObject.prevObject);
-    //console.log(kloeke.find('previousSibling'));
-//    console.log(kloeke.prevObject[0].children[1].children[1].children[0].val());
-//    let kloekeCode = kloeke[0].children[1].children[0]["value"];
-//    console.log("kloekecode: " + kloekeCode);
-/*    if(kloekeCode!='') {
-        console.log('kloekecode not empty')
-        volgordeveld[0].children[1].children[0].value = 1;
-    } else {
-        console.log("page: " + volgordeveld[0]);
-        checkPrev(volgordeveld);
-        // check previous page
-        volgordeveld[0].children[1].children[0].value = checkPrev() + 1;
-    }
-    volgorde = volgordeveld[0].children[1].children[0]["value"];
-    console.log("volgorde: " + volgorde);
-*/
+    console.log('volgorde is set.');
 }
 
 function scanHandler() {
