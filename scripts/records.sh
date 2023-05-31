47b2eb52ebe82aca292e4e749153d6210951d696
Menzo Windhouwer <menzo@windhouwer.nl>
2023-05-31 13:40:50 +0200

    new path to resources

--- scripts/records.sh
+++ scripts/records.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IN="/Users/menzowi/Documents/Projects/VLB/scans/microfiches/V1/cmdi"
+IN="/Users/menzowi/Documents/Projects/VLB/scans/microfiches/V1/cmdi/cmdi"
 OUT="/Users/menzowi/Documents/Projects/VLB/huc-cmdi-app/html/ccf/data/records/inprogress"
 EXT="xml"
 CREATOR="records.sh"
@@ -17,5 +17,5 @@ for REC in `find ${IN} -type f -name "*.${EXT}"`; do
     mkdir -p ${OUT}/md${I}/resources
     ./xsl.sh -s:$REC -xsl:./strip.xsl > ${OUT}/md${I}/metadata/record-1.2.cmdi
     xsltproc ./cmd-record-1_2-to-1_1.xsl ${OUT}/md${I}/metadata/record-1.2.cmdi > ${OUT}/md${I}/metadata/record.cmdi
-    echo "INSERT INTO metadata_records(id,name,profile_id,creator,creation_date) VALUES(${I},\"rec ${I}\",1,\"${CREATOR}\",\"${DATE}\");" >> /Users/menzowi/Documents/Projects/VLB/huc-cmdi-app/html/ccf/data/records/records.sql
+    echo "INSERT INTO metadata_records(id,name,profile_id,creator,creation_date) VALUES(${I},\"rec ${I}\",1,\"${CREATOR}\",\"${DATE}\");" #>> /Users/menzowi/Documents/Projects/VLB/huc-cmdi-app/html/ccf/data/records/records.sql
 done
